cfad889ce8ffabba6152972e8afa10e70fcdf555
ArtemisCasien <nuk385916@gmail.com>
2026-02-13 10:00:00 +0700

    move database info into config.php

--- school/day_3/index.php
+++ school/day_3/index.php
@@ -1,20 +1,7 @@
 <?php
 
 /* PDO Connection */
-$host = 'localhost';
-$db = 'learning';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-try {
-    $pdo = new PDO($dsn, $user, $pass);
-} catch (\PDOException $e) {
-    exit('Connection failed: '.$e->getMessage());
-}
-
+include 'config.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name = $_POST['name'] ?? '';
     $price = $_POST['price'] ?? 0;
