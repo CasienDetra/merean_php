5ffe5e17961eaf38c24d470607e09de00c7e35b7
ArtemisCasien <nuk385916@gmail.com>
2026-03-06 07:58:12 +0700

    formatting

--- school/day_6/index.php
+++ school/day_6/index.php
@@ -1,52 +1,61 @@
 <?php
+
 // OOP
-abstract class vehicle{
-  public $brand;
-  public $name;
-  public $year;
-  public $gas_type;
-  public function __construct($brand,$name,$year,$gas_type){
-  $this->$brand=$brand;
-  $this->$name=$name;
-  $this->$year=$year;
-  $this->$gas_type=$gas_type;
-  }
-  public function getCarDetail(){
-
-  }
-}
-class car extends vehicle {
-  public $model;
-  public function __construct($brand, $name, $year, $gas_type,$model)
-  {
-  
-  $this->$brand=$brand;
-  $this->$name=$name;
-  $this->$year=$year;
-  $this->$gas_type=$gas_type;
-  $this->$model=$model;
-    
-  }
-  /* public function getCarDetail(){ */
-  /*   return parent::getCarDetail() . "gas type: $this->$gas_type"; */
-  /* } */
-}
-abstract class name{} 
-interface Fuel{
-  public function getGasType();
-}
-class moto extends vehicle implements Fuel{
-private $fuel;
-public function __construct($brand,$name,$year,$fuel)
+abstract class vehicle
 {
-  parent::__construct($brand,$name,$year);
-  $this->$fuel=$fuel;
+    public $brand;
+
+    public $name;
+
+    public $year;
+
+    public $gas_type;
+
+    public function __construct($brand, $name, $year, $gas_type)
+    {
+        $this->$brand = $brand;
+        $this->$name = $name;
+        $this->$year = $year;
+        $this->$gas_type = $gas_type;
+    }
+
+    public function getCarDetail() {}
 }
-public function getGasType()
+class car extends vehicle
 {
-  return "gas type is:" . $this->fuel;
-}
-}
+    public $model;
 
+    public function __construct($brand, $name, $year, $gas_type, $model)
+    {
 
-?>
+        $this->$brand = $brand;
+        $this->$name = $name;
+        $this->$year = $year;
+        $this->$gas_type = $gas_type;
+        $this->$model = $model;
+
+    }
+    /* public function getCarDetail(){ */
+    /*   return parent::getCarDetail() . "gas type: $this->$gas_type"; */
+    /* } */
+}
+abstract class name {}
+interface Fuel
+{
+    public function getGasType();
+}
+class moto extends vehicle implements Fuel
+{
+    private $fuel;
+
+    public function __construct($brand, $name, $year, $fuel)
+    {
+        parent::__construct($brand, $name, $year);
+        $this->$fuel = $fuel;
+    }
+
+    public function getGasType()
+    {
+        return 'gas type is:'.$this->fuel;
+    }
+}
